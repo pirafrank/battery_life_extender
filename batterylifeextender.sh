38bd3e92e15424f1a10ede3246705c157f9d5084
Francesco Pira <dev@fpira.com>
2015-05-31 13:23:13 +0200

    license fix

--- batterylifeextender.sh
+++ batterylifeextender.sh
@@ -9,18 +9,18 @@
 #
 # This file is part of battery_life_extender
 #
-# This script is free software: you can redistribute it and/or modify
+# battery_life_extender is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This script is distributed in the hope that it will be useful,
+# battery_life_extender is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this script.  If not, see <http://www.gnu.org/licenses/>.
+# along with battery_life_extender. If not, see <http://www.gnu.org/licenses/>.
 #
 ########################################################################
 
